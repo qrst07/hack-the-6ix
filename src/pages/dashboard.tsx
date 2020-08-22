0e47e6254c090098a23b63d531df1c9369286800
Kesojan <keso1429@gmail.com>
2020-08-22 12:19:23 -0400

    homepage

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import SEO from '../components/SEO';
 import svg from '../images/undraw_online_learning_ao11.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import pic2 from '../images/undraw_taking_notes_tjaf.png';
+import pic3 from '../images/undraw_group_hangout_5gmq.png';
+import pic4 from '../images/undraw_online_learning_ao11.png';
 
 import styled from 'styled-components';
-import { Form, Card, Button } from 'react-bootstrap';
+import { Form, Card, Button, CardDeck } from 'react-bootstrap';
 const HomeEdit = styled.div`
   display: flex;
   background-color: grey;
@@ -16,34 +20,51 @@ const HomeEdit = styled.div`
 const Home = () => {
   return (
     <div>
-      <Card className="text-center">
-        <Card.Header>Welcome</Card.Header>
-        <Card.Body>
-          <Card.Title>Video Conferencing Software</Card.Title>
-          <Card.Text>With supporting text below as a natural lead-in to additional content.</Card.Text>
-          <br />
-          <img src={svg} />
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      <SEO title="Home" keywords={['OAH', 'application', 'react']} />
+      <h1>Home Page</h1>
+      <br></br>
+      <CardDeck>
+        <Card>
+          <Card.Img variant="top" src={pic4} />
+          <Card.Body>
+            <Card.Title>Start a Call</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This content
+              is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={pic2} />
+          <br></br>
+          <Card.Body>
+            <Card.Title>Explore Notes</Card.Title>
 
-      <h1>Sign Up</h1>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted"></Form.Text>
-        </Form.Group>
+            <Card.Text>This card has supporting text below as a natural lead-in to additional content. </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={pic3} />
+          <br></br>
+          <br></br>
+          <br></br>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-      </Form>
+          <Card.Body>
+            <Card.Title>Join a Call</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to additional content.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
     </div>
   );
 };
