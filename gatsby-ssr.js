5ded24bcb20475857f60a3583fce1ed70320c8ee
Anusha Dey <aunosua00@gmail.com>
2020-08-22 03:25:18 -0400

    add env, create room from frontend

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,13 +1,13 @@
 import React from 'react';
 export const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
   setPostBodyComponents([
-    <script key="alertify" src="https://unpkg.com/@daily-co/daily-js" type="text/javascript" crossOrigin="" />,
+    <script key="daily1" src="https://unpkg.com/@daily-co/daily-js" type="text/javascript" crossOrigin="" />,
     <script
-      key="fun_javascript"
+      key="daily2"
       dangerouslySetInnerHTML={{
         __html: `
         callFrame = window.DailyIframe.createFrame();
-        callFrame.join({ url: 'https://notate.daily.co/hello' })
+        callFrame.join({ url: 'https://notate.daily.co/testthis' })
       `,
       }}
     />,
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@paljs/ui/Button';
 import { Card, CardBody } from '@paljs/ui/Card';
 import Col from '@paljs/ui/Col';
 import Row from '@paljs/ui/Row';
@@ -6,15 +7,38 @@ import styled from 'styled-components';
 import SEO from '../components/SEO';
 
 const Home = () => {
+  // TEMPORARY: create room to test
+  var data = JSON.stringify({
+    name: 'testthis',
+  });
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('readystatechange', function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open('POST', 'https://api.daily.co/v1/rooms');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.setRequestHeader('authorization', `Bearer ${process.env.API_KEY}`);
+
+  xhr.send(data);
+
+  // const sendRequest = () => {
+
+  // }
+
   return (
     <>
       <SEO title="Home" keywords={['OAH', 'application', 'react']} />
       <Row>
         <Col breakPoint={{ xs: 12, md: 6 }}>
           <Card>
-            <header>Input status</header>
+            <header>rooms</header>
             <CardBody>
-              <div>here</div>
+              <Button>click</Button>
             </CardBody>
           </Card>
         </Col>
