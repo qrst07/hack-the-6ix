14897c06f0e09608827bb5beff50af154cc74e56
Anusha Dey <aunosua00@gmail.com>
2020-08-23 01:51:24 -0400

    center image

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -7,17 +7,18 @@ import { Link } from 'gatsby';
 import Auth, { Group } from '../../components/Auth';
 import Socials from '../../components/Auth/Socials';
 import SEO from '../../components/SEO';
-import pic1 from '/Users/keso/hack-the-6ix/src/images/118214809_302380157663530_5844143322821461235_n.jpg';
+import pic1 from '../../images/118214809_302380157663530_5844143322821461235_n.jpg';
 export default function Login() {
   const onCheckbox = () => {
     // v will be true or false
   };
   return (
     <>
+      <SEO title="Login" keywords={['OAH', 'application', 'react']} />
       <Auth title="Notate" subTitle="Welcome! Login or Sign Up for free">
-        <img src={pic1} style={{ width: '200px', height: '150px' }} />
-        <SEO title="Login" keywords={['OAH', 'application', 'react']} />
-        <div style={{ alignContent: 'center' }}></div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <img src={pic1} style={{ width: '200px', height: '150px' }} />
+        </div>
         <form>
           <InputGroup fullWidth>
             <input type="email" placeholder="Email Address" />
